acc0cf75c2dff0b39ac8327c76d021ce62ec902f
Zpeugh <zach.peugh@spidasoftware.com>
2016-04-23 15:33:22 -0400

    Changing reuters directory

--- Lab5/lab5.py
+++ Lab5/lab5.py
@@ -1,11 +1,10 @@
 import utilities
 
-REUTERS_DIRECTORY = "../reuters"
 NUM_FILES = 5
 
 SUPPORT = '2'
 CONFIDENCE = '60'
 
-topics, words, topics_set = utilities.preprocess_data(reuters_directory=REUTERS_DIRECTORY, num_files=NUM_FILES)
+topics, words, topics_set = utilities.preprocess_data(num_files=NUM_FILES)
 
 results = utilities.run_ar_classifier(topics, words, topics_set, SUPPORT, CONFIDENCE, .666, 3)
